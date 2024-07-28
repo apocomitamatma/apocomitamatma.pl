3344df12eccaa9c313071f7ea449e4d8a73a997b
bswck <bartoszpiotrslawecki@gmail.com>
2024-07-28 12:26:31 +0200

    Use lambdas for `Icon` event handling

--- front/src/routes/Icon.svelte
+++ front/src/routes/Icon.svelte
@@ -3,18 +3,17 @@
 
 	export let icon;
 	let bounce = false;
-
-	function enter() {
-		bounce = true;
-	}
-
-	function leave() {
-		bounce = false;
-	}
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div on:mouseenter={enter} on:mouseleave={leave}>
+<div
+	on:mouseenter={() => {
+		bounce = true;
+	}}
+	on:mouseleave={() => {
+		bounce = false;
+	}}
+>
 	{#key bounce}
 		<FontAwesomeIcon {bounce} {icon} />
 	{/key}
