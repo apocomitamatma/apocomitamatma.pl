a171e0a5c0ec509747ad9ef3226865697f03bf39
bswck <bartoszpiotrslawecki@gmail.com>
2024-07-30 04:12:22 +0200

    Move floating statistics to its own local component

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script>
-	import { Carousel, FilmCounter, Logo } from '$lib/components';
+	import { Logo } from '$lib/components';
 	import { FiguresSprite } from '$lib/components/sprites';
 
-	/** @type {Array<{category: 'e8' | 'mp' | 'mr', target: string}>} */
-	const categories = [
-		{ category: 'e8', target: 'ósmoklasistów' },
-		{ category: 'mp', target: 'maturzystów<br>(poziom podstawowy)' },
-		{ category: 'mr', target: 'maturzystów<br>(poziom rozszerzony)' }
-	];
+	import FloatingStatistics from './FloatingStatistics.svelte';
 </script>
 
 <svelte:head>
@@ -19,23 +14,7 @@
 	<div class="landing">
 		<div class="landing-item landing-left landing-sprite">
 			<FiguresSprite />
-			<div class="floating-statistics">
-				<Carousel>
-					<div class="carousel-element">
-						<div class="number-display">300+</div>
-						<p>
-							uczniów zadowolonych<br />
-							z <a href="/contact">korepetycji</a><br />
-						</p>
-					</div>
-					{#each categories as { category, target }}
-						<div class="carousel-element">
-							<div class="number-display"><FilmCounter {category} /></div>
-							<p>filmów dla {@html target}</p>
-						</div>
-					{/each}
-				</Carousel>
-			</div>
+			<FloatingStatistics />
 		</div>
 		<div class="landing-item landing-right">
 			<div class="landing-description">
@@ -133,41 +112,6 @@
 		text-wrap: balance;
 		padding-top: 10px;
 		padding-bottom: 10px;
-	}
-
-	.number-display {
-		font-weight: bold;
-		font-size: 3.5em;
-		margin-bottom: -25px;
-		color: var(--theme-yellow);
-	}
-
-	.floating-statistics {
-		filter: brightness(150%);
-		position: absolute;
-		bottom: 20%;
-		height: 100px;
-		margin-bottom: -10%;
-		text-align: center;
-		font-weight: bolder;
-		font-size: var(--layout-emp-font-size);
-		text-shadow:
-			0px 0px 10px #000000,
-			0px 0px 20px #000000,
-			0px 0px 30px #000000;
-	}
-
-	.floating-statistics a {
-		color: var(--theme-yellow);
-	}
-
-	.floating-statistics a:hover {
-		filter: brightness(80%);
-	}
-
-	.carousel-element {
-		display: flex;
-		justify-content: center;
 	}
 
 	@media (max-width: 900px) {
---
+++ front/src/routes/FloatingStatistics.svelte
@@ -0,0 +1,66 @@
+<script>
+	import { Carousel, FilmCounter } from '$lib/components';
+
+	/** @type {Array<{category: 'e8' | 'mp' | 'mr', target: string}>} */
+	const categories = [
+		{ category: 'e8', target: 'ósmoklasistów' },
+		{ category: 'mp', target: 'maturzystów<br>(poziom podstawowy)' },
+		{ category: 'mr', target: 'maturzystów<br>(poziom rozszerzony)' }
+	];
+</script>
+
+<div class="floating-statistics">
+	<Carousel>
+		<div class="carousel-element">
+			<div class="number-display">300+</div>
+			<p>
+				uczniów zadowolonych<br />
+				z <a href="/contact">korepetycji</a><br />
+			</p>
+		</div>
+		{#each categories as { category, target }}
+			<div class="carousel-element">
+				<div class="number-display"><FilmCounter {category} /></div>
+				<p>filmów dla {@html target}</p>
+			</div>
+		{/each}
+	</Carousel>
+</div>
+
+<style>
+	.number-display {
+		font-weight: bold;
+		font-size: 3.5em;
+		margin-bottom: -25px;
+		color: var(--theme-yellow);
+	}
+
+	.floating-statistics {
+		filter: brightness(150%);
+		position: absolute;
+		bottom: 20%;
+		height: 100px;
+		margin-bottom: -10%;
+		text-align: center;
+		font-weight: bolder;
+		font-size: var(--layout-emp-font-size);
+		/* Unfortunately, repeat() is only valid in grid-template-columns context */
+		text-shadow:
+			0px -1px 10px #000000,
+			0px -1px 10px #000000,
+			0px -1px 10px #000000;
+	}
+
+	.floating-statistics a {
+		color: var(--theme-yellow);
+	}
+
+	.floating-statistics a:hover {
+		filter: brightness(80%);
+	}
+
+	.carousel-element {
+		display: flex;
+		justify-content: center;
+	}
+</style>
