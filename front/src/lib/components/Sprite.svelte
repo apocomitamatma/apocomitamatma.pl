320f79e46bff84a4ecc46e599b5e64574a6ddef6
bswck <bartoszpiotrslawecki@gmail.com>
2024-07-30 03:54:24 +0200

    Make sprites transparent

--- front/src/lib/components/Sprite.svelte
+++ front/src/lib/components/Sprite.svelte
@@ -8,11 +8,9 @@
 
 <style>
 	.sprite-image {
-		backdrop-filter: brightness(50%) blur(17px);
 		width: 100%;
 		height: 90%;
 		object-fit: contain;
-		box-shadow: 0px 0px 35px 35px rgba(0, 0, 0, 1);
 		z-index: -1;
 	}
 </style>
